090a51ce242763f1bfeb8484df3161dce5612ddf
Lucas Silva e Pereira <lucaspereira.dev@proton.me>
2023-12-11 18:55:13 -0300

    Dia 1

---
+++ mascotePokemon/Modelos/MethodGET.cs
@@ -0,0 +1,14 @@
+﻿using RestSharp;
+
+namespace mascotePokemon.Modelos;
+
+internal class MethodGET
+{
+    public static RestResponse RequisicaoGet(string url)
+    {
+        var client = new RestClient(url);
+        RestRequest restRequest = new("", Method.Get);
+        var response = client.Execute(restRequest);
+        return response;
+    }
+}
--- mascotePokemon/Program.cs
+++ mascotePokemon/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using mascotePokemon.Modelos;
+using Newtonsoft.Json;
+using RestSharp;
+
+void ExibirOpcoes()
+{
+    Console.WriteLine("Bem vindo ao seu mascote pokemon!!");
+    Console.WriteLine("Escolha a espécie:");
+    var response = MethodGET.RequisicaoGet("https://pokeapi.co/api/v2/pokemon-species/");
+    var EspeciesPokemon = JsonConvert.DeserializeObject<PokemonResult>(response.Content);
+    
+    for (int i = 0; i < EspeciesPokemon.Results.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}.{EspeciesPokemon.Results[i].Name}");
+    }
+
+    Console.Write("Digite sua respota: ");
+    int pokemonEscolhido = int.Parse(Console.ReadLine()!);
+
+    while (true)
+    {
+        if (pokemonEscolhido <= EspeciesPokemon.Results.Count)
+        {
+            var especificacaoPokemon = MethodGET.RequisicaoGet($"https://pokeapi.co/api/v2/pokemon/{pokemonEscolhido}");
+            var especificacaoPokemonConvert = JsonConvert.DeserializeObject<PokemonEspeciesResult>(especificacaoPokemon.Content);
+            Console.WriteLine(especificacaoPokemonConvert);
+            break;
+        }
+        else
+        {
+            Console.WriteLine("\n Opção invalida");
+        }
+    }
+}
+ExibirOpcoes();
